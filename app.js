371526df83256a85832dbccc2580c2a93120e333
Dakota Bryant <bryant.dakota@gmail.com>
2017-04-12 11:23:00 -0500

    finished basic functionality

--- app.js
+++ app.js
@@ -9,35 +9,43 @@ $(function(){
       key:apiKey,
       q:searchTerm
     }
-    
     $.getJSON(endpointURL, query, callback);
   }
-  
-  getData('Pewdiepie', showThumbnail);
-  
-  
+
+
+
+
   function showThumbnail(data){
     let thumbnails = `<p>No Results</p>`;
-    
+
     thumbnails = data.items.map(function(item){
-      return `<img src="${item}" />`
+      return `<img src=" ${item.snippet.thumbnails.high.url} " />`
     })
-    console.log(data);
+    $('.js-container').append(thumbnails);
+  }
+  function clickListener() {
 
   }
+  $('.search-form').on('click', 'button', function(event) {
+    event.preventDefault();
+    $('.js-container').empty();
+    let searchTerm = $('#search-box').val();
+      getData(searchTerm, showThumbnail);
+  })
 
-  
+//item.snippet.thumbnails.high.url
+
 //function displayOMDBSearchData(data) {
 //  var resultElements = '<p>No results</p>';
-//  
+//
 //  if (data.Search) {
 //    resultElements = data.Search.map(function(item) {
 //      return '<p>' + item.Title + '</p>';
 //    });
 //  }
-//  
+//
 //  $('.js-search-results').html(resultElements);
 //}
 
-  
+
 })
